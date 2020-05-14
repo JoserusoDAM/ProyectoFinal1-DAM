0ecb8955269cb3bdc43cee92019efca876b8421b
JoserusoDAM <jorusoalvarez@gmail.com>
2020-05-14 09:00:53 +0200

    Ultimo commit

--- src/Controlador/main.java
+++ src/Controlador/main.java
@@ -4,12 +4,6 @@ package Controlador;
 import Vista.Portada;
 import Vista.Ventanaprincipal;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Jose
@@ -20,7 +14,6 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-                
         new Controlador_ventanaprincipal(new Portada(), new Ventanaprincipal()).iniciar();
         
     }
